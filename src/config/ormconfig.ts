55eb46a6c102cf01931f9456471cc150cf67f7f7
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-01-13 11:53:25 +0000

    feat: Change ormconfig.ts for connect DB

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -2,12 +2,12 @@ import { ConnectionOptions } from "typeorm";
 
 // TODO: Should connect nomakase DB
 const ormconfig: ConnectionOptions = {
-  type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: "postgres",
-  password: "postgres",
-  database: "postgres",
+  type: "mysql",
+  host: process.env.DB_HOST,
+  port: 3306,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   synchronize: true,
   entities: ["src/entity/**/*.ts"],
   migrations: ["src/migration/**/*.ts"],
