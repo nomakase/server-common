9f460191bb1adbf1361d67fcc9d0cd8382419cc5
appano1 <thankfather@naver.com>
2021-01-16 14:14:49 +0900

    feat: Connect DB

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,24 @@ import "reflect-metadata";
 import "./config/config";
 import normalizePort from "./utils/normalizePort";
 import app from "./app";
+import { createConnection } from "typeorm";
+import ormconfig from "./config/ormconfig";
 
 async function main() {
   const port = normalizePort(process.env.PORT);
 
   // TODO: Need to change ormconfig.ts for connect nomakase DB
-  // const connection = await createConnection(ormconfig);
+  try {
+    await createConnection(ormconfig);
+  } catch (err) {
+    console.log(
+      `
+        ################################################
+                  ⚠️ Fail to connect DB ⚠️
+        ################################################
+    ` + err
+    );
+  }
 
   try {
     app.listen(port, () => {
