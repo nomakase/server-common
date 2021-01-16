bdd0011746a20dd11feb36e9528e2733bfbab306
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-01-16 15:44:38 +0000

    feat: Add authenticate middleware

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import express from "express";
+import authenticate from "./middlewares/authenticate";
 import authRouter from "./routes/authRouter";
-import addErrorHandlers from "./routes/errorHandlers";
 import signUpRouter from "./routes/signUpRouter";
+import addErrorHandlers from "./routes/errorHandlers";
 
 const app = express();
 
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 
-app.use("/signUp", signUpRouter);
-
-app.use("/", (_req, _res, next) => {
+app.use("/", (req, _res, next) => {
+  console.log(req.originalUrl);
   next();
 });
 
+// Need to authenticate except for the url starting with "sign".
+app.use(/\/((?!sign).)*/, authenticate);
+
 app.use("/", authRouter);
+app.use("/signUp", signUpRouter);
 
 addErrorHandlers(app);
 export default app;
