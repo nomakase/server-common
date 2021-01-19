98797c9c74b8a7147ea9e60985b2d57c0abd2c73
appano1 <thankfather@naver.com>
2021-01-19 15:50:30 +0900

    feat: Export TypedResponse type

--- src/@types/errorHandlers/index.d.ts
+++ src/@types/errorHandlers/index.d.ts
@@ -2,7 +2,7 @@ import express from "express";
 
 type Overwrite<T, U> = Omit<T, keyof U> & U;
 
-type TypedResponse<T> = Overwrite<
+export type TypedResponse<T> = Overwrite<
   express.Response,
   { json(data: T): TypedResponse<T>; status(code: number): TypedResponse<T> }
 >;
@@ -15,5 +15,4 @@ export type CustomError = Overwrite<Error, { name?: string }> & {
   type: string;
   code: number;
 };
-
 //export type TypedResponse<T> = Omit<express.Response, 'json' | 'status'> & { json(data: T): TypedResponse<T> } & { status(code: number): TypedResponse<T> };
