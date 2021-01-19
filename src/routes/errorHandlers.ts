96ed45173497a771a452bcedf798cbf65af16038
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-01-19 04:20:33 +0000

    feat: Add error types and modify error handlers

--- src/routes/errorHandlers.ts
+++ src/routes/errorHandlers.ts
@@ -1,63 +1,28 @@
 import express from "express";
+import { ErrorResponse, CustomError } from "../@types/errorHandlers";
 
 export default function addErrorHandlers(app: express.Application) {
   /* 404 ERROR */
   app.use((_req, res) => {
     res.status(404).json({
-      message: "Not Found."
+      type: "NotFoundError",
+      status: 404,
+      message: "The requested URL was not found.",
     });
   });
 
-  /* 401 ERROR */
   app.use(
     (
-      err: any,
+      err: CustomError,
       req: express.Request,
-      res: express.Response,
-      next: express.NextFunction
-    ) => {
-      if (err.code == 401) {
-        console.log("Auth Failed - " + err.code + req.originalUrl);
-        res.status(err.code || 401).json({
-          message: "Unauthorized : Access to this resource is denied."
-        });
-      } else {
-        next(err);
-      }
-    }
-  );
-
-  /* 403 ERROR */
-  app.use(
-    (
-      err: any,
-      req: express.Request,
-      res: express.Response,
-      next: express.NextFunction
-    ) => {
-      if (err.code == 403) {
-        console.log("Auth Failed - " + err.code + req.originalUrl);
-        res.status(err.code || 403).json({
-          message: "Forbidden : Access to this resource is denied."
-        });
-      } else {
-        next(err);
-      }
-    }
-  );
-
-  /* 500 ERROR */
-  app.use(
-    (
-      err: any,
-      req: express.Request,
-      res: express.Response,
+      res: ErrorResponse,
       _next: express.NextFunction
     ) => {
-      console.log("Internal Server Error - " + err.code + req.originalUrl);
-      res.status(err.status || 500).json({
-        message:
-          "Internal Server Error : The server has been deserted for a while."
+      console.log(err.code + " " + req.originalUrl);
+      res.status(err.code).json({
+        type: err.type ?? err.name ?? "Internal Server Error",
+        status: err.code ?? 500,
+        message: err.message ?? "The server has been deserted for a while.",
       });
     }
   );
