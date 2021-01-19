1f7ed92ac806969157c5ee18fcb6b10b4131c613
appano1 <thankfather@naver.com>
2021-01-19 15:57:36 +0900

    feat: Add next function and define rest part

--- src/routes/restaurantRouter.ts
+++ src/routes/restaurantRouter.ts
@@ -1,42 +1,55 @@
 import express from "express";
 import { Restaurant } from "../entities/Restaurant";
-import { AppResponse } from "./interface";
 
 const router = express.Router();
 
-router.post("/register", async (req, res: AppResponse) => {
-  const { name, phoneNumber, address, open, close, description, photos }: Partial<Restaurant> = req.body;
+router.post("/register", async (req, res, next) => {
+  const { name, phoneNumber, address, openningHour, breakTime, description, photos }: Partial<Restaurant> = req.body;
 
-  if (!(name && phoneNumber && address && open && close)) {
-    return res.json({
-      success: false,
-      message: "Invalid : Please check json"
-    })
+  if (!(name && phoneNumber && address)) {
+    const error: any = new Error("Name, address, and phone number are required.");
+    error.code = 400;
+    error.type = "Invalid Input";
+
+    next(error);
+    return;
   }
-
 
   const regex = /\d{2,3}-\d{3,4}-\d{4}/;
   if (!regex.exec(phoneNumber)) {
-    return res.json({
-      success: false,
-      message: "Invalid : Please check phone number"
-    })
+    const error: any = new Error("Please check phone number");
+    error.code = 400;
+    error.type = "Invalid Phone Number";
+
+    next(error);
+    return;
   }
 
+  let restaurantId: number;
   try {
-    await Restaurant.insert({
+    const insertResult = await Restaurant.insert({
       name,
       phoneNumber,
       address,
-      open,
-      close,
-      description
+      openningHour,
+      breakTime,
+      description,
+      photos
     })
+    restaurantId = insertResult.identifiers[0].id;
   } catch (err) {
     console.error(err);
+
+    if (err.errno === 1062) {
+      err.code = 500;
+      err.type = "Duplicated Phone Number"
+      err.message = `${phoneNumber} has already taken`
+      next(err);
+    }
+    return;
   }
 
-  return res.json({ success: true })
+  return res.json({ id: restaurantId })
 })
 
 export default router;
