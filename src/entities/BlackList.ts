4741f98554ff83720ac64c62ab8c5df3d6553764
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-02-04 15:29:56 +0000

    feat: Add some constants in BlackList.ts

--- src/entities/BlackList.ts
+++ src/entities/BlackList.ts
@@ -3,21 +3,22 @@ import Redis from "../config/Redis";
 export class BlackList {
     
     public static readonly MAX_REMAINING = 86400; //sec
-
+    public static readonly REASON_REFRESH = "0";
+    public static readonly REASON_NEW_SIGNIN = "1";
+    
     private static readonly ACCESS_TOKEN_PREFIX = "ACCESS:";
     //private static readonly REFRESH_TOKEN_PREFIX = "REFRESH:";
     
-    private static readonly DUMMY = "true";
     private static readonly SET_SUCCESS = "OK";
     private static readonly EXPIRE_SUCCESS = true;
     
     
-    static addAccessToken = async (jti: string, expiresIn: number) => {
+    static addAccessToken = async (jti: string, reason: string, expiresIn: number) => {
         const key = BlackList.ACCESS_TOKEN_PREFIX + jti;
         
         try {
             let res: any = await Promise.resolve()
-                .then(() => Redis.getClient().SET(key, BlackList.DUMMY));
+                .then(() => Redis.getClient().SET(key, reason));
             
             if (res !== BlackList.SET_SUCCESS) {
                 throw new Error("Fail to set value in Redis.");
