990e45870342899fe20bd9b37195681e544f5016
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-02-24 11:36:29 +0000

    fix: Set OAuth2Client as static member OAuth2Client caches the cert, which is used for verifying Google ID token. If OAuth2Client is created newly on every request, the cached cert will be useless, OAuth2Client will fetch the new cert everytime and it is time-consuming. That's why I set OAuth2Client as a static member.

--- src/auth/OAuth/GoogleOAuth.ts
+++ src/auth/OAuth/GoogleOAuth.ts
@@ -2,23 +2,23 @@ import { OAuth2Client, TokenPayload } from "google-auth-library";
 import OAuth from "./interface/OAuth";
 
 export default class GoogleOAuth implements OAuth {
-  private readonly CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
-
-  private userInfo?: TokenPayload;
-  private client = new OAuth2Client(this.CLIENT_ID);
-
   readonly serverName = "google";
 
+  // OAuth2Client must be static.
+  private static readonly CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+  private static client = new OAuth2Client(GoogleOAuth.CLIENT_ID);
+
+  private tokenPayload?: TokenPayload;
+
   async authenticate(token: string) {
-    let ticket: any = {};
-    ticket = await this.client.verifyIdToken({
+    const ticket = await GoogleOAuth.client.verifyIdToken({
       idToken: token,
-      audience: this.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
+      audience: GoogleOAuth.CLIENT_ID, // Specify the CLIENT_ID of the app that accesses the backend
     });
 
-    const userInfo = ticket.getPayload();
-    if (userInfo){
-      this.userInfo = userInfo;
+    const tokenPayload = ticket.getPayload();
+    if (tokenPayload){
+      this.tokenPayload = tokenPayload;
       return true;
     }
     
@@ -26,6 +26,6 @@ export default class GoogleOAuth implements OAuth {
   }
 
   getUserInfo() {
-    return this.userInfo?.email ? this.userInfo.email : null;
+    return this.tokenPayload?.email ? this.tokenPayload.email : null;
   }
 }
