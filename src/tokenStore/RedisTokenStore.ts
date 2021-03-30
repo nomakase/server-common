8c81b21fad2b445e5bb15e6defc9956a7c81da39
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-03-30 19:13:05 +0000

    refactor: Use lowercase Redis command functions

--- src/tokenStore/RedisTokenStore.ts
+++ src/tokenStore/RedisTokenStore.ts
@@ -13,14 +13,14 @@ export default class RedisTokenStore implements TokenStore{
         
         try {
             let res: any = await Promise.resolve()
-                .then(() => Redis.getClient().SET(key, reason));
-            
+                .then(() => Redis.getClient().set(key, reason));
+
             if (res !== RedisTokenStore.SET_SUCCESS) {
                 throw new Error("Fail to set value in Redis.");
             }
             
             res = await Promise.resolve()
-                .then(() => Redis.getClient().EXPIRE(key, RedisTokenStore.PADDING_TIME + expiresIn));
+                .then(() => Redis.getClient().expire(key, RedisTokenStore.PADDING_TIME + expiresIn));
                 
             if (res !== RedisTokenStore.EXPIRE_SUCCESS) {
                 throw new Error("Fail to set expiration time.");
@@ -35,7 +35,7 @@ export default class RedisTokenStore implements TokenStore{
     async findToken(prefix: string, jti: string): Promise<TokenReason>  {
         try {
             return await Promise.resolve().then(
-                () => (<TokenReason><unknown>Redis.getClient().GET(prefix + jti)));
+                () => (<TokenReason><unknown>Redis.getClient().get(prefix + jti)));
         } catch (err) {
             console.error(err);
             throw err;
@@ -45,7 +45,7 @@ export default class RedisTokenStore implements TokenStore{
     async removeToken(prefix: string, jti: string) {
         try {
             const res: any = await Promise.resolve().then(
-                () => Redis.getClient().DEL(prefix + jti));
+                () => Redis.getClient().del(prefix + jti));
 
             if (res !== RedisTokenStore.DEL_SUCCESS) {
                 throw new Error("Fail to delete key in Redis.");
