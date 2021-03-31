90902df21d5fd14ba12942aca75cdb300efb8a5b
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-03-31 16:35:12 +0000

    chore: Update Redis config

--- src/config/Redis/IoRedis.ts
+++ src/config/Redis/IoRedis.ts
@@ -3,17 +3,18 @@ import Redis from "./Redis";
 
 export default class IoRedis extends Redis<ioRedis.Redis> {
     private static readonly redisConfig: ioRedis.RedisOptions = {
+      connectionName: "Nomakase Server Application",
       host: process.env.DB_HOST,
       port: Number(process.env.REDIS_PORT),
       password: process.env.REDIS_PASSWORD,
       retryStrategy: (times) => {
         console.log("Redis connection is lost with somehow reason.");
         console.log("attempt: " + times);
-        if (times > 10) {
+        if (times > 5) {
           return undefined;
         }
   
-        return Math.min(times * 100, 3000);
+        return Math.min(times * 2000, 5000);
       }
     };
   
@@ -23,11 +24,12 @@ export default class IoRedis extends Redis<ioRedis.Redis> {
           Redis.client = new ioRedis(IoRedis.redisConfig);
 
           Redis.client.on("error", (err) => {
+            console.log("error on connecting to Redis.")
             reject(err);
           });
   
           Redis.client.on("ready", () => {
-            console.log("redis_lib: ioredis")
+            console.log("redis_lib: ioredis.")
             resolve();
           });
   
