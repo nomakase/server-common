5db5638ea252ff74a6333bf825b5e78a93b214b7
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-04-02 07:54:29 +0000

    feat: Add uploadTo()

--- src/utils/upload.ts
+++ src/utils/upload.ts
@@ -2,20 +2,26 @@ import multer, { diskStorage } from "multer";
 import path from "path";
 import fs from "fs";
 
-const storage = diskStorage({
-  destination: (_req, _file, cb) => {
-    const destination = path.join(__dirname, "../public/images");
-
-    if (!fs.existsSync(destination)) {
-      fs.mkdirSync(destination, { recursive: true });
+export const mkStorage = (dirName: string = "") => {
+  return diskStorage({
+    destination: (_req, _file, cb) => {
+      const destination = path.join(__dirname, "../public/images/" + dirName);
+  
+      if (!fs.existsSync(destination)) {
+        fs.mkdirSync(destination, { recursive: true });
+      }
+      cb(null, destination);
+    },
+    filename: (_, file, cb) => {
+      const { ext, name } = path.parse(file.originalname);
+      const fileName = name + "_" + Date.now() + ext;
+      cb(null, fileName);
     }
-    cb(null, destination);
-  },
-  filename: (_, file, cb) => {
-    const { ext, name } = path.parse(file.originalname);
-    const fileName = name + "_" + Date.now() + ext;
-    cb(null, fileName);
-  }
-})
+  })
+}
 
-export const upload = multer({ storage });
+export const upload = multer({ storage : mkStorage() });
+
+export const uploadTo = (dirName: string) => {
+  return multer({ storage : mkStorage(dirName) });
+}
