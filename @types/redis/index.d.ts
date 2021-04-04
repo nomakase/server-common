050762c7c6bbbd36e90ab7d86e8c638f943b9be0
LeeDongGeon1996 <secmatth1996@gmail.com>
2021-04-04 06:17:25 +0000

    feat: Add commandTimeout option with updating ioredis

--- @types/redis/index.d.ts
+++ @types/redis/index.d.ts
@@ -2,3 +2,5 @@ import nodeRedis from "redis";
 import ioRedis from "ioredis";
 
 export type RedisClient = nodeRedis.RedisClient | ioRedis.Redis;
+
+export type IoRedisOptions = ioRedis.RedisOptions & { commandTimeout?: number };
--- src/config/Redis/IoRedis.ts
+++ src/config/Redis/IoRedis.ts
@@ -1,8 +1,9 @@
 import ioRedis from "ioredis";
 import Redis from "./Redis";
+import { IoRedisOptions } from "@custom-types/redis";
 
 export default class IoRedis extends Redis<ioRedis.Redis> {
-    private static readonly redisConfig: ioRedis.RedisOptions = {
+    private static readonly redisConfig: IoRedisOptions = {
       connectionName: "Nomakase Server Application",
       host: process.env.DB_HOST,
       port: Number(process.env.REDIS_PORT),
@@ -15,14 +16,15 @@ export default class IoRedis extends Redis<ioRedis.Redis> {
         }
   
         return Math.min(times * 2000, 5000);
-      }
+      },
+      commandTimeout: 5000
+      
     };
   
     createConnection() {
       return new Promise<void>((resolve, reject) => {
         try {
           Redis.client = new ioRedis(IoRedis.redisConfig);
-
           Redis.client.on("error", (err) => {
             console.log("error on connecting to Redis.")
             reject(err);
